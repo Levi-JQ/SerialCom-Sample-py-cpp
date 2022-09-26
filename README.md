68d729fb22f9e328f606e1dbbdcacb3a0d4cc151
Levi-JQ <1960307306@qq.com>
2022-09-26 12:01:10 +0800

    修改README

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # SerialCom-Sample-py-cpp
-定义python类，调用c++动态链接库完成数据转换，再发送到串口
+（1）定义python类，调用c++动态链接库完成数据转换，再发送到串口
 
-主要代码位于python_serial文件夹，其他文件夹为个人记录代码学习过程所用
+参考链接：
+[Python如何向C指针传递数据](https://blog.csdn.net/DeliaPu/article/details/126859941)
+
+（2）主要代码位于python_serial文件夹，其他文件夹为个人记录代码学习过程所用
+
+（3）VScode中launch.json和tasks.json该怎么编写，有何作用，可参考.vscode文件夹
+
+参考链接：
+[VScode 调试 tasks.json和launch.json的设置（超详细）](https://blog.csdn.net/qq_59084325/article/details/125662393?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2~default~CTRLIST~Rate-1-125662393-blog-124612782.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2~default~CTRLIST~Rate-1-125662393-blog-124612782.pc_relevant_default&utm_relevant_index=2)
--- pytorch_study/test.py
+++ pytorch_study/test.py
@@ -2,5 +2,30 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+def forward(x):
+    return w[0]*(x**2) + w[1]*x+w[2]
+
+def lossfunc(x,y):
+    y_pred = forward(x)
+    return (y-y_pred)**2
+
 if __name__ == "__main__":
     print("success")
+    w = torch.Tensor([2.0,-1.0,3.0])
+    w.requires_grad = True
+    x_data = [1.0,2.0,3.0,-1.0,-2.0]
+    y_data = [3.0,7.0,13.0,1.0,3.0]
+    for epoch in range(100):
+        for x,y in zip(x_data,y_data):
+            l = lossfunc(x,y)
+            l.backward()
+            print ('\tgrad:', x, y, w)
+            w.data = w.data - 0.01* w.grad.data
+            w.grad.data.zero_()
+        print("progress:",epoch,l.item())
+    print("perdict(after training)",4,forward(4).item())
+
+
+
+
+
